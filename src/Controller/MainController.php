b5dcc191e93e35221c3b46dae60958b8f34256ca
Oskar <94227871+Oskii0201@users.noreply.github.com>
2022-07-07 20:00:04 +0200

    integration with nbp api

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,15 +4,38 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annatation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+// use Symfony\APP\Controller\Exception;
 
 class MainController extends AbstractController{
 
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
     public function nbp(){
 
-        return $this->json([
-            'message' => 'Welcome to your new controller!'
-        ]);
+        $url = 'http://api.nbp.pl/api/exchangerates/tables/A?format=json';
 
+        $json = $this->send_request($url);
+        echo $json;die();
+
+    }
+
+    public function send_request($url){
+
+        if(empty($url)){
+            return false;
+        }
+        $response = $this->client->request(
+            'GET', $url
+        );
+        $content = $response->getContent();
+
+        return $content;
     }
 }
 
